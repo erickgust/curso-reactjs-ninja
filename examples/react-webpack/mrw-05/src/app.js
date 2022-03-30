41b92c55b85fb57ec4a666629809372c82fef2d2
Erick Gustavo <erick.stazs@gmail.com>
2022-03-30 20:22:30 -0300

    Add mrw-05

---
+++ examples/react-webpack/mrw-05/src/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const React = require('react');
+
+const Title = React.createClass({
+  render() {
+    return React.createElement('h1', null, 'Título');
+  }
+});
+
+module.exports = Title;
