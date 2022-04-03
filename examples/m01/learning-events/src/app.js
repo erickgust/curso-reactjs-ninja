4207805932adcd39596ab249ba4fe31829f3c610
Erick Gustavo <erick.stazs@gmail.com>
2022-04-03 15:34:25 -0300

    Add learning-events

---
+++ examples/m01/learning-events/src/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import React, { Component } from 'react'
+import Button from './button'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>
+          <span>Span</span>
+          Botão
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default App
