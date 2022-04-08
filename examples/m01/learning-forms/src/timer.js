890350894d7a88bde40998efa77c98b7420dc1df
Erick Gustavo <erick.stazs@gmail.com>
2022-04-07 21:09:51 -0300

    Introduction to forms

---
+++ examples/m01/learning-forms/src/timer.js
@@ -0,0 +1,51 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class Timer extends Component {
+  constructor () {
+    console.log('constructor timer')
+    super()
+    this.state = {
+      time: 0
+    }
+
+    this.timer
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log('componentWillReceiveProps timer', this.props, nextProps)
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    return this.state.time === nextState.time
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+    console.log('componentWillUpdate timer', this.props, nextProps)
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    console.log('componentDidUpdate timer', this.props, prevProps)
+  }
+
+  componentDidMount () {
+    this.timer = setInterval(() => this.setState({
+      time: this.state.time + 1
+    }), 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  render () {
+    return (
+      <div>
+        Timer: {this.state.time}
+      </div>
+    )
+  }
+}
+
+export default Timer
