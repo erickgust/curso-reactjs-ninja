a07ca852e70e67c208a7f012f64372e7e2db261c
Erick Gustavo <erick.stazs@gmail.com>
2022-04-08 11:50:11 -0300

    Add select and option examples

--- examples/m01/learning-forms/src/app.js
+++ examples/m01/learning-forms/src/app.js
@@ -6,8 +6,7 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'Initial value',
-      checked: false
+      value: '3'
     }
   }
 
@@ -15,25 +14,14 @@ class App extends Component {
     return (
       <div>
         <form>
-          <input
-            type='text'
+          <select
             value={this.state.value}
-            onChange={(e) => (this.setState({ value: e.target.value }))}
-          />
-
-          <label>
-            <input
-              type='checkbox'
-              checked={this.state.checked}
-              onChange={(e) => (this.setState({ checked: e.target.checked }))}
-            />
-            Checkbox
-          </label>
-
-          <input type='radio' name='radio' id='radio-1' value='1' defaultChecked />
-          <label htmlFor='radio-1'>Radio 1</label>
-          <input type='radio' name='radio' id='radio-2' value='2' />
-          <label htmlFor='radio-2'>Radio 2</label>
+            onChange={(e) => this.setState({ value: e.target.value })}
+          >
+            <option value='1'>Option 1</option>
+            <option value='2'>Option 2</option>
+            <option value='3'>Option 3</option>
+          </select>
         </form>
       </div>
     )
