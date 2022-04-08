9a50233cc7ea1916fa3587b4e443bb7059013019
Erick Gustavo <erick.stazs@gmail.com>
2022-04-08 16:30:13 -0300

    Add events example

--- examples/m01/learning-forms/src/app.js
+++ examples/m01/learning-forms/src/app.js
@@ -13,11 +13,20 @@ class App extends Component {
   render () {
     return (
       <div>
-        <form>
-          <textarea
-            value={this.state.value}
-            onChange={(e) => this.setState({ value: e.target.value })}
-          />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            console.log(e)
+          }}
+          onChange={(e) => {
+            console.log('name', e.target.name)
+            console.log('value', e.target.value)
+          }}
+        >
+          <input type='text' name='name' />
+          <input type='email' name='email' />
+
+          <button type='submit'>Send</button>
         </form>
       </div>
     )
