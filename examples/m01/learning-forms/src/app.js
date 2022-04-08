3ce9afc1910e082b8a89c42f50a8d69ddc5f5f91
Erick Gustavo <erick.stazs@gmail.com>
2022-04-07 23:54:02 -0300

    Add checkbox and radio examples

--- examples/m01/learning-forms/src/app.js
+++ examples/m01/learning-forms/src/app.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'Initial value'
+      value: 'Initial value',
+      checked: false
     }
   }
 
@@ -19,6 +20,20 @@ class App extends Component {
             value={this.state.value}
             onChange={(e) => (this.setState({ value: e.target.value }))}
           />
+
+          <label>
+            <input
+              type='checkbox'
+              checked={this.state.checked}
+              onChange={(e) => (this.setState({ checked: e.target.checked }))}
+            />
+            Checkbox
+          </label>
+
+          <input type='radio' name='radio' id='radio-1' value='1' defaultChecked />
+          <label htmlFor='radio-1'>Radio 1</label>
+          <input type='radio' name='radio' id='radio-2' value='2' />
+          <label htmlFor='radio-2'>Radio 2</label>
         </form>
       </div>
     )
