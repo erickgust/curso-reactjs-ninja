8d2749199eab1f1018ecdafa6e5ab55d0fd1b299
Erick Gustavo <erick.stazs@gmail.com>
2022-04-10 14:01:35 -0300

    Add GitHub app markup

--- examples/m01/github-app/src/app.js
+++ examples/m01/github-app/src/app.js
@@ -4,7 +4,43 @@ import React from 'react'
 
 const App = () => (
   <div className='app'>
-    GitHub App
+    <div className='search-bar'>
+      <input type='search' placeholder='Digite o usuário' />
+    </div>
+
+    <div className='git-user'>
+      <img src='https://avatars.githubusercontent.com/u/83595333?v=4' />
+      <h1 className='username'>
+        <a href='https://github.com/erickgust'>Erick Gustavo</a>
+      </h1>
+
+      <ul className='user-info'>
+        <li>Repositórios: 13</li>
+        <li>Seguidores: 0</li>
+        <li>Seguindo: 3</li>
+      </ul>
+
+      <div className='user-actions'>
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
+      </div>
+
+      <div className='user-repos'>
+        <h2>Repositórios:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+      <div className='user-stars'>
+        <h2>Favoritos:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 )
 
