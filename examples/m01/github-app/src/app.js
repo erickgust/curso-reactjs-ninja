9f229c2d7ff4d94cbe8a793891197e359541638e
Erick Gustavo <erick.stazs@gmail.com>
2022-04-13 16:35:02 -0300

    Set user state

--- examples/m01/github-app/src/app.js
+++ examples/m01/github-app/src/app.js
@@ -4,8 +4,47 @@ import React, { Component } from 'react'
 import AppContent from './components/app-content'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      userInfo: null,
+      repos: [],
+      stars: []
+    }
+  }
+
+  handleSearch (e) {
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+
+    if (keyCode === ENTER) {
+      fetch(`https://api.github.com/users/${value}`)
+        .then(req => req.json())
+        .then(user => {
+          this.setState({
+            userInfo: {
+              name: user.name,
+              avatar: user.avatar_url,
+              login: user.login,
+              repos: user.public_repos,
+              followers: user.followers,
+              following: user.following
+            }
+          })
+        })
+    }
+  }
+
   render () {
-    return <AppContent />
+    return (
+      <AppContent
+        userInfo={this.state.userInfo}
+        repos={this.state.repos}
+        stars={this.state.stars}
+        handleSearch={(e) => this.handleSearch(e)}
+      />
+    )
   }
 }
 
