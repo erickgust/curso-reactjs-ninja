c395380cb1b7ba3a33f30c0f1b8c9bc756d95bb1
Erick Gustavo <erick.stazs@gmail.com>
2022-04-13 16:33:19 -0300

    Add handleSearch function

--- examples/m01/github-app/src/components/search.js
+++ examples/m01/github-app/src/components/search.js
@@ -1,14 +1,20 @@
 'use strict'
 
+import { PropTypes } from 'prop-types'
 import React from 'react'
 
-const Search = () => (
+const Search = ({ handleSearch }) => (
   <div className='search-bar'>
     <input
       type='search'
       placeholder='Digite o usuário'
+      onKeyUp={handleSearch}
     />
   </div>
 )
+
+Search.propTypes = {
+  handleSearch: PropTypes.func.isRequired
+}
 
 export default Search
