5ec93a538c730c0f7d977eaa4d566bba17576898
Erick Gustavo <erick.stazs@gmail.com>
2022-04-30 21:42:41 -0300

    Fix dev environment on github app

--- examples/m02/working-with-assets/github-app/src/components/repos/index.js
+++ examples/m02/working-with-assets/github-app/src/components/repos/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './repos.css'
 
-const Repos = ({title, repos }) => (
+const Repos = ({ title, repos }) => (
   <div className='repos-list'>
     <h2>{title}:</h2>
     <ul>
