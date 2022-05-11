63b1ac3102a0ebfb0229364e76426966316972f5
Erick Gustavo <erick.stazs@gmail.com>
2022-05-10 22:35:13 -0300

    Test pagination exceptions

--- examples/m02/applications/github-app/src/utils/pagination/index.js
+++ examples/m02/applications/github-app/src/utils/pagination/index.js
@@ -1,18 +1,26 @@
 'use strict'
 
-function centerItems ({ activePage, total }) {
-  if (activePage - 1 <= 0) {
-    return 1
-  }
-
-  if (activePage === total) {
-    return activePage - 2
-  }
-
-  return activePage - 1
+function centerItems ({ total, activePage }) {
+  return activePage - 1 <= 0
+    ? 1
+    : activePage === total
+      ? activePage - 2
+      : activePage - 1
 }
 
-function pagination ({ total, activePage }) {
+function isNumber (value) {
+  return typeof value === 'number'
+}
+
+function pagination ({ total = 1, activePage = 1 } = {}) {
+  if (!isNumber(total)) {
+    throw new TypeError('"total" must be a number')
+  }
+
+  if (!isNumber(activePage)) {
+    throw new TypeError('"activePage" must be a number')
+  }
+
   if (total <= 5) {
     return Array.from({ length: total }, (item, index) => index + 1)
   }
--- examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
+++ examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
@@ -65,3 +65,32 @@ it('pagination({ total: 10, activePage: 5 }) should return [1, "...", 4, 5, 6, "
   const result = [1, '...', 4, 5, 6, '...', 10]
   expect(pagination(params)).toEqual(result)
 })
+
+it('pagination({ total: 10 }) should return [1, 2, 3, "...", 10]', () => {
+  const params = { total: 10 }
+  const result = [1, 2, 3, '...', 10]
+  expect(pagination(params)).toEqual(result)
+})
+
+it('pagination({}) should return [1]', () => {
+  const params = {}
+  const result = [1]
+  expect(pagination(params)).toEqual(result)
+})
+
+it('pagination() should return [1]', () => {
+  const result = [1]
+  expect(pagination()).toEqual(result)
+})
+
+it('pagination({ total: "hello", activePage: 1 }) must throw an error', () => {
+  const params = { total: "hello", activePage: 1 }
+  const msg = '"total" must be a number'
+  expect(() => pagination(params)).toThrow(new TypeError(msg))
+})
+
+it('pagination({ total: 5, activePage: "bye" }) must throw an error', () => {
+  const params = { total: 5, activePage: "bye" }
+  const msg = '"activePage" must be a number'
+  expect(() => pagination(params)).toThrow(new TypeError(msg))
+})
