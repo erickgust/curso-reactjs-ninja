8eeab25d45f3d9c9351a241af5bbf48838b4347f
Erick Gustavo <erick.stazs@gmail.com>
2022-05-10 23:41:05 -0300

    Add the utils alias

--- examples/m02/applications/github-app/storybook/webpack.config.js
+++ examples/m02/applications/github-app/storybook/webpack.config.js
@@ -1,14 +1,21 @@
 'use strict'
 
+const path = require('path')
 const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config')
 
 module.exports = function (config, env) {
   const newConfig = webpackConfig(config, env)
-  newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat([{
+  newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat({
     test: /\.js$/,
     exclude: /node_modules/,
     loader: 'standard'
-  }])
+  })
+
+  newConfig.resolve = {
+    alias: {
+      utils: path.join(__dirname, '..', 'src', 'utils')
+    }
+  }
 
   return newConfig
 }
