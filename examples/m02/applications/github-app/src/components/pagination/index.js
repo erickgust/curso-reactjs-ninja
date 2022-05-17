d4d5f8a04e76bb6113b3587db48414fc7103c7a7
Erick Gustavo <erick.stazs@gmail.com>
2022-05-16 23:13:48 -0300

    Add PropTypes

--- examples/m02/applications/github-app/src/components/pagination/index.js
+++ examples/m02/applications/github-app/src/components/pagination/index.js
@@ -1,14 +1,29 @@
 'use strict'
 
 import React from 'react'
+import PropTypes from 'prop-types'
 import pagination from 'utils/pagination'
+import Page from './page'
 
-const Pagination = ({ total, activePage }) => (
+const Pagination = ({ total, activePage, pageLink, onClick }) => (
   <ul>
     {pagination({ total, activePage }).map((page, index) => (
-      <li key={index}>{page}</li>
+      <li key={index}>
+        <Page page={page} pageLink={pageLink.replace('%page%, page')} onClick={onClick} />
+      </li>
     ))}
   </ul>
 )
+
+Pagination.defaultProps = {
+  pageLink: ''
+}
+
+Pagination.propTypes = {
+  total: PropTypes.number,
+  activePage: PropTypes.number,
+  pageLink: PropTypes.string,
+  onClick: PropTypes.func
+}
 
 export default Pagination
