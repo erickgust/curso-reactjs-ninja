fe39a7a361481229df7e6d66fa7e294f8f1e15ea
Erick Gustavo <erick.stazs@gmail.com>
2022-05-16 23:14:15 -0300

    Add more stories

--- examples/m02/applications/github-app/src/components/pagination/pagination.story.js
+++ examples/m02/applications/github-app/src/components/pagination/pagination.story.js
@@ -6,5 +6,19 @@ import Pagination from '.'
 
 storiesOf('<Pagination />', module)
   .add('without props', () => (
-    <Pagination total={10}/>
+    <Pagination />
+  ))
+
+  .add('with total and activePage', () => (
+    <Pagination total={10} activePage={5} />
+  ))
+
+  .add('with page link', () => (
+    <Pagination total={10} activePage={5} pageLink='https://mypage.com/page/%page%' />
+  ))
+
+  .add('with callback', () => (
+    <Pagination total={10} activePage={5} pageLink='https://mypage.com/page/%page%' onClick={(page) => {
+      window.alert(page)
+    }}/>
   ))
