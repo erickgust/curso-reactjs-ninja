2d3e841b20e75597c14a9052c16362339db4d216
Erick Gustavo <erick.stazs@gmail.com>
2022-05-17 19:37:03 -0300

    Add color red in activePage

--- examples/m02/applications/github-app/src/components/pagination/index.js
+++ examples/m02/applications/github-app/src/components/pagination/index.js
@@ -8,15 +8,20 @@ import Page from './page'
 const Pagination = ({ total, activePage, pageLink, onClick }) => (
   <ul>
     {pagination({ total, activePage }).map((page, index) => (
-      <li key={index}>
-        <Page page={page} pageLink={pageLink.replace('%page%, page')} onClick={onClick} />
+      <li key={index} style={activePage === page ? { color: '#f00' } : null}>
+        <Page
+          page={page}
+          pageLink={pageLink.replace('%page%, page')}
+          onClick={onClick}
+        />
       </li>
     ))}
   </ul>
 )
 
 Pagination.defaultProps = {
-  pageLink: ''
+  pageLink: '',
+  activePage: 1
 }
 
 Pagination.propTypes = {
