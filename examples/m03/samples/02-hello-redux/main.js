7090c04cff2197c15c0c828756c2b8c49b01c40c
Erick Gustavo <erick.stazs@gmail.com>
2022-06-13 15:44:19 -0300

    Create store and use methods

---
+++ examples/m03/samples/02-hello-redux/main.js
@@ -0,0 +1,17 @@
+export const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1
+    case 'DECREMENT': return state - 1
+  }
+
+  return state
+}
+
+const { createStore } = Redux
+const store = createStore(counter)
+
+store.subscribe(() => { console.log('value', store.getState()) })
+
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'DECREMENT' })
