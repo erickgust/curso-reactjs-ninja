3b7823cf60f4821ca902ccb7b9520c957cfd78c7
Erick Gustavo <erick.stazs@gmail.com>
2022-06-18 23:36:57 -0300

    test: use variables instead of strings

--- examples/m03/samples/10-react-counter/src/redux-flow/reducers/counters/counters.test.js
+++ examples/m03/samples/10-react-counter/src/redux-flow/reducers/counters/counters.test.js
@@ -1,4 +1,4 @@
-import { counters } from './index'
+import { counters, ADD_COUNTER, DECREMENT, INCREMENT, REMOVE_COUNTER } from './index'
 
 it('Counters should be a function', () => {
   expect(typeof counters).toBe('function')
@@ -6,56 +6,56 @@ it('Counters should be a function', () => {
 
 it('Should add a counter', () => {
   const input = []
-  const action = { type: 'ADD_COUNTER' }
+  const action = { type: ADD_COUNTER }
   const output = [0]
   expect(counters(input, action)).toEqual(output)
 })
 
 it('Should add another counter', () => {
   const input = [0, 1]
-  const action = { type: 'ADD_COUNTER' }
+  const action = { type: ADD_COUNTER }
   const output = [0, 1, 0]
   expect(counters(input, action)).toEqual(output)
 })
 
 it('Should remove a counter', () => {
   const input = [0, 1, 0]
-  const action = { type: 'REMOVE_COUNTER', index: 1 }
+  const action = { type: REMOVE_COUNTER, index: 1 }
   const output = [0, 0]
   expect(counters(input, action)).toEqual(output)
 })
 
 it('Should remove another counter', () => {
   const input = [0]
-  const action = { type: 'REMOVE_COUNTER', index: 0 }
+  const action = { type: REMOVE_COUNTER, index: 0 }
   const output = []
   expect(counters(input, action)).toEqual(output)
 })
 
 it('Should increment a counter', () => {
   const input = [0, 3, 2]
-  const action = { type: 'INCREMENT', index: 0 }
+  const action = { type: INCREMENT, index: 0 }
   const output = [1, 3, 2]
   expect(counters(input, action)).toEqual(output)
 })
 
 it('Should increment another counter', () => {
   const input = [1, 0, 1]
-  const action = { type: 'INCREMENT', index: 1 }
+  const action = { type: INCREMENT, index: 1 }
   const output = [1, 1, 1]
   expect(counters(input, action)).toEqual(output)
 })
 
 it('Should decrement a counter', () => {
   const input = [0, 2, 2]
-  const action = { type: 'DECREMENT', index: 1 }
+  const action = { type: DECREMENT, index: 1 }
   const output = [0, 1, 2]
   expect(counters(input, action)).toEqual(output)
 })
 
 it('Should decrement another counter', () => {
   const input = [1, 2]
-  const action = { type: 'DECREMENT', index: 1 }
+  const action = { type: DECREMENT, index: 1 }
   const output = [1, 1]
   expect(counters(input, action)).toEqual(output)
 })
@@ -73,5 +73,4 @@ it('Should return initial state if last state is undefined', () => {
   const output = []
   expect(counters(input, action)).toEqual(output)
 })
-
 
