b5a7511beb5f8caea6c58ecfde23ffd0c8bd15f1
Erick Gustavo <erick.stazs@gmail.com>
2022-07-08 21:54:22 -0300

    M04 - Spa concepts - History API (onpopstate)

---
+++ examples/m04/02-spa-concepts/03/src/main.ts
@@ -0,0 +1,34 @@
+const $links = document.querySelectorAll('a')!
+const $h1 = document.querySelector('h1')!
+
+type State = {
+  url: string
+}
+
+type Titles = {
+  [key: string]: string
+}
+
+$links.forEach($link => $link.addEventListener('click', handleClick))
+
+window.onpopstate = (e) => {
+  navigation({url: e.state.url})
+}
+
+function navigation(state: State) {
+  const titles: Titles = {
+    '/': 'Home',
+    '/about': 'Sobre'
+  }
+
+  $h1.textContent = titles[state.url]
+}
+
+function handleClick(e: MouseEvent) {
+    e.preventDefault()
+    const element = e.target as HTMLAnchorElement
+    const url = element.getAttribute('href')
+    const state = {url}
+    history.pushState(state, '', url)
+    navigation({url: state.url ?? '/'})
+}
